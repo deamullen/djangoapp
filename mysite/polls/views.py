1b4cd4b36073cd652cc2951811a7dc84a203677b
AndreaAkiko <deamullen@gmail.com>
2016-02-15 14:10:10 -1000

    added image for  weather condition

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,6 +8,7 @@ from django.views.generic import View
 import json as simplejson
 import urllib2 
 from urllib2 import Request
+import re
 
 from .models import Choice, Question
 
@@ -48,7 +49,7 @@ def detail(request, question_id):
 
 def get_weather_list():
     weather_list = []
-    search_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22honolulu%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    search_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%2C%20item.description%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22honolulu%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
     req = Request(search_url)
     raw = urllib2.urlopen(req)
     js = raw.readlines()
@@ -56,15 +57,18 @@ def get_weather_list():
 
     item = js_object['query']['results']['channel']
     city = item['location']['city']
-    item = js_object['query']['results']['channel']['item']['condition']
-    date = item['date']
-    temp = item['temp']
-    condition = item['text']
-    thisweather = weather(city, temp, condition, date)
+    item = js_object['query']['results']['channel']['item']
+    date = item['condition']['date']
+    temp = item['condition']['temp']
+    condition = item['condition']['text']
+    description = js_object['query']['results']['channel']['item']['description']
+    description = re.findall('\\n(.*?)\<br', description)
+
+    thisweather = weather(city, temp, condition, description[0], date)
 
     weather_list.append(thisweather)
 
-    search_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22sao%20paulo%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    search_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%2C%20item.description%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22sao%20paulo%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
     req = Request(search_url)
     raw = urllib2.urlopen(req)
     js = raw.readlines()
@@ -76,11 +80,14 @@ def get_weather_list():
     date = item['date']
     temp = item['temp']
     condition = item['text']
-    thisweather = weather(city, temp, condition, date)
+    description = js_object['query']['results']['channel']['item']['description']
+    description = re.findall('\\n(.*?)\<br', description)
+
+    thisweather = weather(city, temp, condition, description[0], date)
 
     weather_list.append(thisweather)
 
-    search_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22london%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    search_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%2C%20item.description%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22london%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
     req = Request(search_url)
     raw = urllib2.urlopen(req)
     js = raw.readlines()
@@ -92,15 +99,19 @@ def get_weather_list():
     date = item['date']
     temp = item['temp']
     condition = item['text']
-    thisweather = weather(city, temp, condition, date)
+    description = js_object['query']['results']['channel']['item']['description']
+    description = re.findall('\\n(.*?)\<br', description)
+
+    thisweather = weather(city, temp, condition, description[0], date)
 
     weather_list.append(thisweather)
 
     return weather_list
 
 class weather(object):
-    def __init__(self, city, temp, condition, date):
+    def __init__(self, city, temp, condition, description, date):
         self.city = city
         self.temp = temp
         self.condition = condition
         self.date = date
+        self.description = description
