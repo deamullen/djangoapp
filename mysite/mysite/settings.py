690a5f9964ed8dcc87002b523a704b756ce4119d
AndreaAkiko <deamullen@gmail.com>
2016-02-19 21:35:12 -1000

    modified settings for new url

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'andrea.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
