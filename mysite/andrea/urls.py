729ed60093cf5950f6caf17cdb0af70ff1f0c268
andrea <andrea@192.168.0.3>
2016-06-07 19:00:57 -1000

    removed around the world

--- mysite/andrea/urls.py
+++ mysite/andrea/urls.py
@@ -7,7 +7,6 @@ app_name = 'andrea'
 urlpatterns = [
 		url(r'^$', views.index, name='index'),
     url(r'^index/', views.index, name='index'),
-    url(r'^around-the-world/', views.world, name='world'),
     url(r'^contact/', views.contact, name='contact'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- mysite/andrea/views.py
+++ mysite/andrea/views.py
@@ -15,11 +15,6 @@ from .models import Choice, Question
 def index(request):
     return render(request, 'andrea/index.html')
 
-
-def world(request):
-    return render(request, 'andrea/around-the-world.html', {'weather_list' : get_weather_list()})
-
-
 def contact(request):
     return render(request, 'andrea/contact.html')
 
@@ -45,71 +40,3 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'andrea/detail.html', {'question': question})
 
-def get_weather_list():
-    weather_list = []
-    search_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%2C%20item.description%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22honolulu%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    req = Request(search_url)
-    raw = urllib2.urlopen(req)
-    js = raw.readlines()
-    js_object = simplejson.loads(js[0])
-
-    item = js_object['query']['results']['channel']
-    city = item['location']['city']
-    item = js_object['query']['results']['channel']['item']
-    date = item['condition']['date']
-    temp = item['condition']['temp']
-    condition = item['condition']['text']
-    description = js_object['query']['results']['channel']['item']['description']
-    description = re.findall('\\n(.*?)\<br', description)
-
-    thisweather = weather(city, temp, condition, description[0], date)
-
-    weather_list.append(thisweather)
-
-    search_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%2C%20item.description%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22sao%20paulo%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    req = Request(search_url)
-    raw = urllib2.urlopen(req)
-    js = raw.readlines()
-    js_object = simplejson.loads(js[0])
-
-    item = js_object['query']['results']['channel']
-    city = item['location']['city']
-    item = js_object['query']['results']['channel']['item']['condition']
-    date = item['date']
-    temp = item['temp']
-    condition = item['text']
-    description = js_object['query']['results']['channel']['item']['description']
-    description = re.findall('\\n(.*?)\<br', description)
-
-    thisweather = weather(city, temp, condition, description[0], date)
-
-    weather_list.append(thisweather)
-
-    search_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%2C%20location%2C%20item.description%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22london%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    req = Request(search_url)
-    raw = urllib2.urlopen(req)
-    js = raw.readlines()
-    js_object = simplejson.loads(js[0])
-
-    item = js_object['query']['results']['channel']
-    city = item['location']['city']
-    item = js_object['query']['results']['channel']['item']['condition']
-    date = item['date']
-    temp = item['temp']
-    condition = item['text']
-    description = js_object['query']['results']['channel']['item']['description']
-    description = re.findall('\\n(.*?)\<br', description)
-
-    thisweather = weather(city, temp, condition, description[0], date)
-
-    weather_list.append(thisweather)
-
-    return weather_list
-
-class weather(object):
-    def __init__(self, city, temp, condition, description, date):
-        self.city = city
-        self.temp = temp
-        self.condition = condition
-        self.date = date
-        self.description = description
